3b055899d0a563bd3ea1b752cf5371174841733d
TobeHyo <hyosang.kim@tobesoft.com>
2020-04-08 15:54:33 +0900

    first

---
+++ src/test/java/com/tobesoft/overseas/nexacro17/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobesoft.overseas.nexacro17.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
